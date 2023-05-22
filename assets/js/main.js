2821e5d2205d958f960fb897b525cc04c023ffcb
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-05-22 16:28:04 +0200

    Fix api issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
 import { getWeather } from './weather.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('getWeatherBtn').addEventListener('click', getWeather);
-});
+document.getElementById('getWeatherBtn').addEventListener('click', getWeather);
