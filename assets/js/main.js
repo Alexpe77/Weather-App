f75b91475fc14b75d65657bef722f5ecab2ef1a0
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-05-22 17:00:28 +0200

    Change style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 import { getWeather } from './weather.js'
 
 document.getElementById('getWeatherBtn').addEventListener('click', getWeather);
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      getWeather();
+    }
+  });
